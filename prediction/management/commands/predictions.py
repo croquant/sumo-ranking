b61bf03d01c96e45845c18a313f97898791e92c7
Maxime Desmet Vanden Stock <maxime.desmet@easypost.eu>
2024-05-04 12:10:22 +0200

    pr suggestions

--- prediction/management/commands/predictions.py
+++ prediction/management/commands/predictions.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import itertools
 import math
 import random
@@ -11,11 +12,15 @@ from sumoapi.client import SumoApiClient
 sumoapi = SumoApiClient()
 
 
-def win_prob(r1, r2):
-    rating_1 = (r1.glicko.rating - 1500) / 173.7178
-    rd_1 = r1.glicko.rd / 173.7178
-    rating_2 = (r2.glicko.rating - 1500) / 173.7178
-    rd_2 = r2.glicko.rd / 173.7178
+def get_precomputed_rikishi(rikishi):
+    transformed_rating = (rikishi.glicko.rating - 1500) / 173.7178
+    transformed_rd = rikishi.glicko.rd / 173.7178
+    return transformed_rating, transformed_rd
+
+
+def win_prob(precomputed_r1, precomputed_r2):
+    rating_1, rd_1 = precomputed_r1
+    rating_2, rd_2 = precomputed_r2
 
     return 1 / (
         1 + math.exp(-1 * math.sqrt(rd_1**2 + rd_2**2) * (rating_1 - rating_2))
@@ -35,17 +40,32 @@ class Command(BaseCommand):
         )
 
         records = dict()
+        precomputed_r = dict()
         for r in rikishis:
             records[r.name] = {"wins": 0, "obj": r}
+            precomputed_r[r.name] = get_precomputed_rikishi(r)
 
         matchups = list(itertools.combinations(rikishis, 2))
 
-        monte_carlo = 10000
-        for i in range(monte_carlo):
+        def simulate_matchups():
+            local_records = {r.name: {"wins": 0, "obj": r} for r in rikishis}
             for match in matchups:
-                p1 = win_prob(match[0], match[1])
+                p1 = win_prob(
+                    precomputed_r[match[0].name], precomputed_r[match[1].name]
+                )
                 winner = random.choices(match, [p1, 1 - p1])
-                records[winner[0].name]["wins"] += 1
+                local_records[winner[0].name]["wins"] += 1
+            return local_records
+
+        monte_carlo = 10000
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(simulate_matchups) for _ in range(monte_carlo)
+            ]
+            for future in concurrent.futures.as_completed(futures):
+                results = future.result()
+                for name, data in results.items():
+                    records[name]["wins"] += data["wins"]
 
         ranking = dict(
             sorted(records.items(), key=lambda item: -item[1]["wins"])

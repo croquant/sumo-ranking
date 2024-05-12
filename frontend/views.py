26d02777be98f356a36d5559ed2d6056028a8144
Maxime Desmet Vanden Stock <maxime.desmet@easypost.eu>
2024-05-12 16:12:38 +0200

    remove ranomized chart

--- frontend/views.py
+++ frontend/views.py
@@ -75,14 +75,18 @@ class PredictionView(ListView):
 def chart_view(request):
     data_1 = []
     data_2 = []
+    data_3 = []
+    data_4 = []
 
     rikishi = (
         Rikishi.objects.prefetch_related("glicko")
         .exclude(Q(rank=None) | Q(intai__isnull=False))
-        .order_by("?")
+        .order_by("-glicko__rating")
     )
     rikishi_1 = rikishi[0]
     rikishi_2 = rikishi[1]
+    rikishi_3 = rikishi[2]
+    rikishi_4 = rikishi[3]
 
     history_1 = (
         GlickoHistory.objects.select_related("basho")
@@ -94,19 +98,35 @@ def chart_view(request):
         .filter(glicko=rikishi_2.glicko)
         .order_by("basho")
     )
+    history_3 = (
+        GlickoHistory.objects.select_related("basho")
+        .filter(glicko=rikishi_3.glicko)
+        .order_by("basho")
+    )
+    history_4 = (
+        GlickoHistory.objects.select_related("basho")
+        .filter(glicko=rikishi_4.glicko)
+        .order_by("basho")
+    )
 
     all_dates = set()
     for basho_glicko in history_1:
         all_dates.add(basho_glicko.basho.end_date.isoformat())
     for basho_glicko in history_2:
         all_dates.add(basho_glicko.basho.end_date.isoformat())
+    for basho_glicko in history_3:
+        all_dates.add(basho_glicko.basho.end_date.isoformat())
+    for basho_glicko in history_4:
+        all_dates.add(basho_glicko.basho.end_date.isoformat())
 
-    vd1 = 0
-    vd2 = 0
+    vd1 = "NaN"
+    vd2 = "NaN"
+    vd3 = "NaN"
+    vd4 = "NaN"
     for date in sorted(all_dates):
         vd1 = next(
             (
-                bg.rating
+                round(bg.rating)
                 for bg in history_1
                 if bg.basho.end_date.isoformat() == date
             ),
@@ -116,7 +136,7 @@ def chart_view(request):
 
         vd2 = next(
             (
-                bg.rating
+                round(bg.rating)
                 for bg in history_2
                 if bg.basho.end_date.isoformat() == date
             ),
@@ -124,15 +144,38 @@ def chart_view(request):
         )
         data_2.append(vd2)
 
+        vd3 = next(
+            (
+                round(bg.rating)
+                for bg in history_3
+                if bg.basho.end_date.isoformat() == date
+            ),
+            vd3,
+        )
+        data_3.append(vd3)
+
+        vd4 = next(
+            (
+                round(bg.rating)
+                for bg in history_4
+                if bg.basho.end_date.isoformat() == date
+            ),
+            vd4,
+        )
+        data_4.append(vd4)
+
     return render(
         request,
         "charts/base.html",
         {
-            "title": f"{rikishi_1.name} vs {rikishi_2.name}",
             "name_1": rikishi_1.name,
             "data_1": data_1,
             "name_2": rikishi_2.name,
             "data_2": data_2,
+            "name_3": rikishi_3.name,
+            "data_3": data_3,
+            "name_4": rikishi_4.name,
+            "data_4": data_4,
             "labels": sorted(all_dates),
         },
     )

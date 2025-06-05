2535218ecea881d9a3d3295fb14ad882ca4200cd
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2025-06-05 08:44:00 +0200

    Fix get_all_rikishi loop termination

--- sumoapi/client.py
+++ sumoapi/client.py
@@ -10,12 +10,14 @@ class SumoApiClient:
     def get_all_rikishi(self):
         all_rikishi = []
         skip = 0
-        records = []
-        while records is not None:
-            all_rikishi.extend(records)
+        records = None
+        while True:
             endpoint = f"/rikishis?intai=true&limit=1000&skip={skip * 1000}"
             response = requests.get(f"{BASE_URL}{endpoint}")
             records = json.loads(response.text)["records"]
+            if not records:
+                break
+            all_rikishi.extend(records)
             skip += 1
         return all_rikishi
 

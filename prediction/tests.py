172edfd8fbedce19a6f20285955ff371d29f6425
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2025-06-05 08:47:19 +0200

    Add tests for rank conversion

--- prediction/tests.py
+++ prediction/tests.py
@@ -1,1 +1,17 @@
-# Create your tests here.
+from django.test import SimpleTestCase
+
+from rikishi.utils import convert_long_to_short_rank
+
+
+class ConvertLongToShortRankTests(SimpleTestCase):
+    def test_maegashira_one_east(self):
+        self.assertEqual(
+            convert_long_to_short_rank("Maegashira 1 East"),
+            "m1e",
+        )
+
+    def test_ozeki(self):
+        self.assertEqual(convert_long_to_short_rank("Ozeki"), "o")
+
+    def test_empty_string_returns_none(self):
+        self.assertIsNone(convert_long_to_short_rank(""))
